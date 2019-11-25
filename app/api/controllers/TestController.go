8bb3ba66fdc3b279080304fedac5bc25e83533bf
dadi01hongliang <lianghong@dadi01.com>
2019-11-25 12:28:29 +0800

    test webhook

--- app/api/controllers/TestController.go
+++ app/api/controllers/TestController.go
@@ -1,45 +1,44 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
-	"web-go-skeleton/app/api/models"
 	"fmt"
-	"web-go-skeleton/app/api/self_errors"
+	"github.com/gin-gonic/gin"
 	"log"
-	"web-go-skeleton/library/glog"
-	"web-go-skeleton/app/api/components"
-	"time"
 	"math/rand"
+	"time"
+	"web-go-skeleton/app/api/components"
+	"web-go-skeleton/app/api/models"
+	"web-go-skeleton/app/api/self_errors"
+	"web-go-skeleton/library/glog"
 )
 
 type Params struct {
-	Name string `form:"name" json:"name" binding:"required"`
-	Id uint `form:"id" json:"id" binding:"required,min=5"`
-	Items []Item `form:"items" json:"items" binding:"required,dive"`//dive用于让slice、array、map、嵌套的struct深入下一层
+	Name  string `form:"name" json:"name" binding:"required"`
+	Id    uint   `form:"id" json:"id" binding:"required,min=5"`
+	Items []Item `form:"items" json:"items" binding:"required,dive"` //dive用于让slice、array、map、嵌套的struct深入下一层
 }
 
 type Item struct {
 	ProductId uint `form:"product_id" json:"product_id" binding:"required"`
-	Number uint `form:"number" json:"number" binding:"required"`
+	Number    uint `form:"number" json:"number" binding:"required"`
 }
 
-
-func TestIndex(ctx *gin.Context){
+func TestIndex(ctx *gin.Context) {
 	var seasons []models.Season
 	res := models.DB.Find(&seasons)
 
 	if res.Error != nil {
-		ReturnError(ctx,self_errors.ErrServiceNotAvailable)
+		ReturnError(ctx, self_errors.ErrServiceNotAvailable)
 		return
 	}
 
-	ReturnSuccess(ctx,seasons)
+	ReturnSuccess(ctx, seasons)
 	return
 }
 
-func TestPost(ctx *gin.Context){
+func TestPost(ctx *gin.Context) {
 	var params Params
-	if !ValidateParams(ctx,&params) {
+	if !ValidateParams(ctx, &params) {
 		return
 	}
 
@@ -50,24 +49,24 @@ func TestPost(ctx *gin.Context){
 	return
 }
 
-func TestLog(ctx *gin.Context)  {
-	fmt.Fprintln(gin.DefaultWriter,"aaaaaaaabbbb")
+func TestLog(ctx *gin.Context) {
+	fmt.Fprintln(gin.DefaultWriter, "aaaaaaaabbbb")
 	log.Println("log.Println")
 	glog.Error("test glog error")
 	glog.Info("test glog info")
 	return
 }
 
-func TestRedis(ctx *gin.Context)  {
+func TestRedis(ctx *gin.Context) {
 	//err := components.Redis.Set("hello", "world", 0).Err()
 	//if err != nil {
 	//	glog.Error(err)
 	//	ReturnError(ctx,self_errors.ErrServiceNotAvilable)
 	//}
-	var ch = make(chan string,10)
-	var list = make([]string,0,100)
+	var ch = make(chan string, 10)
+	var list = make([]string, 0, 100)
 
-	for i:=0;i<100;i++ {
+	for i := 0; i < 100; i++ {
 		go func(i int) {
 			randNum := rand.Intn(10)
 			time.Sleep(time.Duration(randNum) * time.Second)
@@ -76,20 +75,20 @@ func TestRedis(ctx *gin.Context)  {
 				glog.Error(err)
 			}
 
-			glog.Infof("%d-%d-%s",i,randNum,val)
-			ch<-val
+			glog.Infof("%d-%d-%s", i, randNum, val)
+			ch <- val
 		}(i)
 	}
 
-	for i:=0;i<100;i++{
-		list = append(list,<-ch)
+	for i := 0; i < 100; i++ {
+		list = append(list, <-ch)
 	}
 
 	ReturnSuccess(ctx)
 	return
 }
 
-func Migration(ctx *gin.Context)  {
+func Migration(ctx *gin.Context) {
 	res := models.DB.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&models.Season{})
 	if res.Error != nil {
 		ctx.JSON(200, gin.H{
@@ -97,6 +96,6 @@ func Migration(ctx *gin.Context)  {
 		})
 	}
 
-	ctx.String(200,"create table success")
+	ctx.String(200, "create table success")
 	return
 }
